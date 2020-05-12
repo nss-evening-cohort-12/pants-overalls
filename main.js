642cda4a3c0f6bb3577c4a6a969f3a26494d13ad
Jim Browning <jamalebrowning@gmail.com>
2020-05-12 18:43:04 -0500

    wip

--- main.js
+++ main.js
@@ -120,3 +120,46 @@ const items = [
         availableSizes: ['X-Small', 'Small', 'Medium', 'Large', 'X-Large', 'XX-Large']
     }
 ]
+// print to dom function
+const printToDom = (selector, textToPrint) => {
+  document.querySelector(selector).innerHTML = textToPrint;
+};
+// end print to dom
+
+const buildChildCards = (arr) => {
+  let domString = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].category === 'Child') {   
+    domString += `
+                 <div class="card" style="width: 18rem;">
+                   <img class="card-img-top" src="..." alt="Card image cap">
+                   <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                   </div>
+                   <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                   </ul>
+                   <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                   </div>
+                 </div>`
+
+  }
+}
+  
+  printToDom('#childCards', domString)
+
+const clickEvents = () => {
+  
+};
+
+const init = () => {
+  buildChildCards(items)
+  clickEvents();
+};
+
+init();
